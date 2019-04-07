a9d02e6b90f85b4a6bf59d38b5b9e853b29d6f60
HantzSPS <36486223+HantzSPS@users.noreply.github.com>
2019-04-07 15:49:42 -0400

    Add files via upload

---
+++ trees.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, send_from_directory, render_template
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+# This is an API meant to serve some housing price index data
+@app.route('/trees/<string:boroname>/<string:spc_common>')
+def return_trees_data(boroname, spc_common):
+
+    # Read in raw data
+    raw_data = pd.read_json(
+        'https://data.cityofnewyork.us/resource/nwxe-4ae8.json')
+    
+
+    # Filter based on borough and common species
+    filtered_data = raw_data.loc[(raw_data['boroname'] == boroname) & (raw_data['spc_common'] == spc_common),
+                                 ['boroname', 'spc_common', 'health']]
+
+    # Deal with datetime
+    #filtered_data['DATE'] = pd.to_datetime(filtered_data['DATE'])
+
+    # Pivot so it's easier to build our json. Data now looks like this:
+    # | Date | High | Middle | Low |
+    # (Date is the index)
+    #filtered_data = filtered_data.pivot(columns='Tier', index='DATE', values='HPI')
+
+    # Build our json, then return it with jsonify
+    filtered_data_json = {
+        #'Date': filtered_data.index.map(lambda x: x.strftime(format='%Y-%m')).tolist(),
+        'Borough': filtered_data['boroname'].tolist(),
+        'Species': filtered_data['spc_common'].tolist(),
+        'Health': filtered_data['health'].tolist()
+    }
+
+    return jsonify(filtered_data_json)
+
+
+# This routing allows us to view index.html
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# This routing allows us to load local Javascript
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
